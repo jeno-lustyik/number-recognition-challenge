29f5c39c3f3c287856fdd844629164ab26e71e0b
Jeno Lustyik <lustyik.jeno@gmail.com>
2022-04-22 16:17:33 +0200

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 import sys
-
+import cv2 as cv
 import processing
+import torchvision.transforms
 from model import Classifier
 import torch as T
 import torch.nn as nn
@@ -8,6 +10,7 @@ import torch.nn.functional as F
 from torch import optim
 from torchvision import datasets, transforms
 from helper_func import view_classify
+from torchvision.io import read_image
 
 model = Classifier()
 model.load_state_dict(T.load('checkpoint_10.pth'))
@@ -17,3 +20,13 @@ impath = sys.argv[1]
 cont = processing.get_cont(impath)
 cont, box = processing.sort_cont(cont)
 processing.write_images(cont)
+
+for img in os.listdir('img'):
+    img = read_image(f'img/{img}')
+    img = img[None]
+    img = img.type('torch.FloatTensor')
+    with T.no_grad():
+        logits = model.forward(img)
+    ps = F.softmax(logits, dim=1)
+
+    view_classify(img, ps, version='MNIST')
